8a786e05fda6e088d7cd308d0d702bcee033c518
Nagy Bálint <120385002+Ehh910@users.noreply.github.com>
2025-05-29 21:26:25 +0200

    Add files via upload

---
+++ KajakKenu/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KajakKenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { List<Kolcsonzes> Kolcsonzesek = new List<Kolcsonzes>();
+            InitializeComponent();
+
+            foreach (var item in File.ReadAllLines(@"..\..\..\src\kolcsonzes.txt").Skip(1)) 
+            {
+                Kolcsonzesek.Add(new Kolcsonzes(item));
+            }
+
+           DataGridhaha.ItemsSource = Kolcsonzesek;
+
+
+
+        }
+    }
+}
